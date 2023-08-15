bbad0ed38d2026e2dde90b32262f420453957dc4
Kicka3 <kirill_ozornin@mail.ru>
2023-08-15 18:27:17 +0500

    Logic of posts

--- index.js
+++ index.js
@@ -7,29 +7,55 @@ const morgan = require("morgan");
 const userRoute = require('./routes/users');
 const authRoute = require('./routes/auth');
 const postRoute = require('./routes/posts');
+const multer = require('multer');
+const path = require("path");
 
 
 dotenv.config();
+
 async function connectToDatabase() {
-  try {
-    await mongoose.connect(process.env.MONGO_URL);
-    console.log('Соединение с MongoDB установлено');
-  } catch (error) {
-    console.error('Ошибка подключения к MongoDB:', error);
-  }
+   try {
+      await mongoose.connect(process.env.MONGO_URL);
+      console.log('Соединение с MongoDB установлено');
+   } catch (error) {
+      console.error('Ошибка подключения к MongoDB:', error);
+   }
 }
+
 connectToDatabase();
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 
 //middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use('/api/users', userRoute);
+
+const storage = multer.diskStorage({
+   destination: (req, file, cb) => {
+      cb(null, "public/images");
+   },
+   filename: (req, file, cb) => {
+      cb(null, req.body.name);
+   }
+});
+
+const upload = multer({storage});
+app.post("/api/upload", upload.single("file"), (req, res) => {
+   try {
+      return res.status(200).json("File uploaded successfully.")
+   } catch (err) {
+      res.status(508)
+      console.log(err)
+   }
+});
+
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
 
-
 app.listen(8800, () => {
-  console.log('Backend server is running');
+   console.log('Backend server is running');
 });
